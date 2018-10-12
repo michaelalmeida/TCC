07fbb415b2d30fc7cb49e95529af53d398d8dd82
Michael <michaelralmeida@gmail.com>
2018-10-12 20:19:22 -0300

    Request microphone permission

---
+++ platforms/browser/www/js/checkpermission.js
@@ -0,0 +1,13 @@
+(function(){
+  var permissions = cordova.plugins.permissions;
+
+  permissions.requestPermission(permissions.RECORD_AUDIO, success, error);
+
+  function error() {
+    console.warn('Audio recorder permission is not turned on');
+  }
+
+  function success( status ) {
+    if( !status.hasPermission ) error();
+  }
+ })();
