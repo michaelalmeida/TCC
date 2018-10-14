7b417168029486e556347fd6a3c25b4a7f9d2a96
Michael <michaelralmeida@gmail.com>
2018-10-14 10:47:26 -0300

    Win screen

--- www/js/game.js
+++ www/js/game.js
@@ -21,17 +21,21 @@ playGame.prototype = {
     game.load.image("sky", "assets/sprites/sky.png");
     game.load.tilemap("level_0001", "assets/maps/level_0001.json", null, Phaser.Tilemap.TILED_JSON);
     game.load.image("deadly", "assets/maps/tiles/deadly.png");
-    game.load.image("congrats", "assets/sprites/congrats.png");
+    game.load.image("hands", "assets/sprites/hands_congrats.png");
     // volume bar
     game.load.image("vol_bar_01", "assets/sprites/vol_bar_01.png");
     game.load.image("vol_bar_02", "assets/sprites/vol_bar_02.png");
     game.load.image("vol_bar_03", "assets/sprites/vol_bar_03.png");
     game.load.image("vol_bar_04", "assets/sprites/vol_bar_04.png");
     game.load.image("vol_bar_05", "assets/sprites/vol_bar_05.png");
+    // confete
+    game.load.image("confete_blue", "assets/sprites/confete_blue.png");
+    game.load.image("confete_yellow", "assets/sprites/confete_yellow.png");
+    game.load.image("confete_rose", "assets/sprites/confete_rose.png");
   },
   create: function () {
     // Text 
-    var style = { font: "25px Arial", fill: "#ff0044", align: "center" };
+    var style = { font: "36px Arial", fill: "#ff0044", align: "center" };
     mic = new p5.AudioIn();
     // Game scale (size wxh)
     game.scale.pageAlignHorizontally = true;
@@ -53,8 +57,8 @@ playGame.prototype = {
     this.emitter.minParticleScale = 0.5;
     this.emitter.maxParticleScale = 1;
     // Text for log mic get level
-    this.text = game.add.text(game.world.centerX, game.world.centerY, "Level do microfone", style);
-    this.text.anchor.set(0.1);
+    //this.text = game.add.text(game.world.centerX, game.world.centerY, "Level do microfone", style);
+    //this.text.anchor.set(0.1);
     // Set the airnplane paper
     this.airplane = game.add.sprite(50, 250, "airplane");
     this.airplane.anchor.set(0.5);
@@ -66,10 +70,8 @@ playGame.prototype = {
     playImg.anchor.setTo(-0.5, 0.5);
     playImg.alpha = 0;
     game.add.tween(playImg).to({ alpha: 1 }, 500, "Linear", true);
-    // Image shawn when the user win
-    winImage = game.add.sprite(50, game.height / 2, "congrats");
-    winImage.anchor.setTo(-0.6, 0.5);
-    winImage.alpha = 0;
+    
+    //winImage.alpha = 0;
     // Volume bar
     vol_bar_01 = game.add.sprite(0, 0, "vol_bar_01");
     vol_bar_02 = game.add.sprite(0, 0, "vol_bar_02");
@@ -98,7 +100,7 @@ playGame.prototype = {
     vol = mic.getLevel(); 
 
     if ((vol * 100 > 2) && (vol * 100 < 10)) {
-      this.airplane.body.velocity.y = -20;
+      this.airplane.body.velocity.y = -10;
       vol_bar_03.alpha = 1;
     } else if ((vol != 0) && (vol * 100 < 1)) {
       vol_bar_01.alpha = 1;
@@ -117,13 +119,24 @@ playGame.prototype = {
   winGame: function () {
     if (countWin == 1) {
       mic.stop();
-      this.text.setText("Você ganhou! Levou:" + counter);
+      //this.text = game.add.text(game.world.centerX, game.world.centerY, "Parabéns:" + counter, this.style);
+      //this.text.anchor.set(0.1);
+      // Confete
+      emitter = game.add.emitter(game.world.centerX, -200, 200);
+      emitter.makeParticles(['confete_blue', 'confete_yellow', 'confete_rose']);
+      emitter.start(false, 5000, 20);
+      // Image shawn when the user win
+      winImage = game.add.sprite(game.height / 2, -200, "hands");
+      winImage.anchor.setTo(-0.1, 0.5);
+      game.add.tween(winImage).to( { y: game.height / 2 }, 4000, Phaser.Easing.Bounce.Out, true);
     }
   },
   updateCounter: function() {
     counter++;
-    this.text.setText(counter);
-},
+  },
+  statisticLog: function() {
+    // Function used to save the game statistic
+  },
   update: function () {
     // Call the engineOn fuction
     this.engineOn();
@@ -157,7 +170,7 @@ playGame.prototype = {
       if (this.airplane.x > game.width + this.airplane.width) {
         countWin++;
         this.winGame();
-        game.time.events.add(Phaser.Timer.SECOND * 5, function () {
+        game.time.events.add(Phaser.Timer.SECOND * 10, function () {
           game.state.start("PlayGame");
         }, this);
       }
