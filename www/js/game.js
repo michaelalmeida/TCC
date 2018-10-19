0e9c1a7ca53565acde55e9de6794bef9f63560f4
Michael <michaelralmeida@gmail.com>
2018-10-19 20:33:34 -0300

    Back to the form in the same page

--- www/js/game.js
+++ www/js/game.js
@@ -84,7 +84,7 @@ playGame.prototype = {
     //game.input.onDown.add(this.startLevel, this);
     this.gameOver = false;
     
-    button = game.add.button(645, game.height / 2, 'button', this.openPlayerName, this, 1, 0, 0);
+    button = game.add.button(game.height / 2, 490, 'button', this.openPlayerName, this, 1, 0, 0);
     
     //winImage.alpha = 0;
     // Volume bar
